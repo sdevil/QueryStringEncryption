1dc711e1aa4d6da771b0bc8b72f49b590f3006c3
sdevil <sdevilt@gmail.com>
2017-06-14 14:45:16 +1200

    initial projects

---
+++ QueryStringEncryption/Common/Encryption.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace QueryStringEncryption.Common {
+    public class Encryption {
+        #region Encryption/decryption
+
+        private const string EncryptionKey = "575c9fe5cad3595a";
+        private const string EncryptionIV = "4fdd2bc7f92a1322";
+
+        /// <summary>
+        /// Encrypts any string using the Rijndael algorithm.
+        /// </summary>
+        /// <param name="inputText">The string to encrypt.</param>
+        /// <returns>A Base64 encrypted string.</returns>
+        public static string Encrypt(string inputText) {
+            byte[] Key = Encoding.UTF8.GetBytes(EncryptionKey);
+            byte[] IV = Encoding.UTF8.GetBytes(EncryptionIV);
+
+            // Check arguments.
+            if (inputText == null || inputText.Length <= 0)
+                throw new ArgumentNullException("inputText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged()) {
+                rijAlg.Mode = CipherMode.CFB;
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream()) {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)) {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt)) {
+                            //Write all data to the stream.
+                            swEncrypt.Write(inputText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            // Return the encrypted bytes from the memory stream.
+            return Convert.ToBase64String(encrypted.ToArray());
+        }
+
+        /// <summary>
+        /// Decrypts a previously encrypted string.
+        /// </summary>
+        /// <param name="inputText">The encrypted string to decrypt.</param>
+        /// <returns>A decrypted string.</returns>
+        public static string Decrypt(string inputText) {
+            var cipherText = Convert.FromBase64String(inputText);
+            byte[] Key = Encoding.UTF8.GetBytes(EncryptionKey);
+            byte[] IV = Encoding.UTF8.GetBytes(EncryptionIV);
+
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("inputText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an RijndaelManaged object
+            // with the specified key and IV.
+            using (RijndaelManaged rijAlg = new RijndaelManaged()) {
+                rijAlg.Mode = CipherMode.CFB;
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText)) {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)) {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt)) {
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+        #endregion
+    }
+}
